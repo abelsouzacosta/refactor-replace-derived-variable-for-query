194e3d1c4e8437212d36c775385f91af5e95b32c
Abel Junior <abel.costa@webjump.com.br>
2022-12-25 05:13:15 -0300

    chore: remove comments

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,3 @@
-// todo: [refactor] - replace temp varioable with query
-// we have to refacotr this code so that production is never modified
-// first we should create a function to calculate production
-
 export class ProductionPlan {
   protected adjustments: any[];
 
