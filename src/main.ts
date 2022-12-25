94a4cfbf23db6f5b6cd4b4cc9329400f9247c62e
Abel Junior <abel.costa@webjump.com.br>
2022-12-25 04:54:01 -0300

    chore: modifying code to match with book

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+// todo: [refactor] - replace temp varioable with query
+// we have to refacotr this code so that production is never modified
+// first we should create a function to calculate production
+
 export class ProductionPlan {
   protected adjusments: any[];
 
-  constructor(protected production: number) {
+  constructor(protected production: number = 0) {
     this.adjusments = [];
   }
 
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -2,16 +2,16 @@ import { ProductionPlan } from "../src/main";
 
 describe("ProductionPlan", () => {
   it("Should create a paln and has production the same amount of the argument value given in the constructor", () => {
-    let productionPlan = new ProductionPlan(23);
+    let productionPlan = new ProductionPlan(0);
 
-    expect(productionPlan.getProduction()).toEqual(23);
+    expect(productionPlan.getProduction()).toEqual(0);
   });
 
   it("Should modify the production with the amount given in the adjusment object using applyAdjusment method", () => {
-    let productionPlan = new ProductionPlan(23);
+    let productionPlan = new ProductionPlan(0);
 
     productionPlan.applyAdjustment({ amount: 23 });
 
-    expect(productionPlan.getProduction()).toEqual(46);
+    expect(productionPlan.getProduction()).toEqual(23);
   });
 });
